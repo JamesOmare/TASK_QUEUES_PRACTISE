245ee7670f5238141fbef5720724efb2fb50a2e2
JamesOmare <jamesomare922@gmail.com>
2022-06-16 00:30:04 +0300

    added rq_dashboard functionality

--- main/app/__init__.py
+++ main/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 import redis
 from rq import Queue
+import rq_dashboard
 
 app = Flask(__name__)
+app.config.from_object(rq_dashboard.default_settings)
+app.register_blueprint(rq_dashboard.blueprint, url_prefix = '/rq')
 
 r = redis.Redis()
 q = Queue(connection=r)
