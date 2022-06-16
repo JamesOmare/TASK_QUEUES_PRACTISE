c86d5a06b853d94e4d965e01abf8d390877e266e
JamesOmare <jamesomare922@gmail.com>
2022-06-16 22:35:12 +0300

    added image uploads and redis queue

--- main/app/views.py
+++ main/app/views.py
@@ -1,7 +1,10 @@
+from email import message
 from app import app, q
-from flask import render_template, request
-from app.tasks import count_words
+from flask import render_template, request, flash
+from app.tasks import count_words, create_image_set
 from time import strftime
+import secrets, os
+
 
 @app.route('/')
 def index():
@@ -20,3 +23,33 @@ def add_task():
         message = f"Task queued at {task.enqueued_at.strftime('%a, %d %b %Y %H:%M:%S')}. {q_len} jobs queued"
 
     return render_template('add_task.html', message = message, jobs = jobs)
+
+
+
+app.config['SECRET_KEY'] = 'e7320fksue8909@Hdeowie'
+app.config['UPLOAD_DIRECTORY'] = '/home/james/Desktop/PYthon_Projects/NEW_PROJECTS/TASK_QUEUES/main/app/static/img/uploads'
+
+@app.route('/upload_image', methods = ['GET', 'POST'])
+def upload_image():
+    message = None
+    if request.method == 'POST':
+        image = request.files['image']
+        image_dir_name = secrets.token_hex(16)
+
+        os.mkdir(os.path.join(app.config['UPLOAD_DIRECTORY'], image_dir_name))
+        image.save(os.path.join(app.config['UPLOAD_DIRECTORY'], image_dir_name, image.filename))
+
+        image_dir = os.path.join(app.config['UPLOAD_DIRECTORY'], image_dir_name)
+
+        q.enqueue(create_image_set, image_dir, image.filename)
+
+        flash("Image uploaded and sent for resizing", 'success')
+
+        message = f"/image/{image_dir_name}/{image.filename.split('.')[0]}"
+        
+
+    return render_template('upload_image.html', message = message)
+
+@app.route('/image/<dir>/<img>')
+def view_image(dir, img):
+    return render_template('view_image.html', dir = dir, img = img)
